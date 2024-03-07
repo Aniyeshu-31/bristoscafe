10a79716eb1a987c355b7d1114d0c62f89fa4ca8
Aniyeshu-31 <aniyeshu31@gmail.com>
2024-03-08 01:16:36 +0530

    updated!!

---
+++ app/components/HeaderComponent/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+import React from 'react'
+import styles from './HeaderComponent.module.css'
+import Card from '../card/page'
+import Image from '@/node_modules/next/image'
+import menu from '../../../public/Images/menu.png'
+import line from '../../../public/Images/sliderImg.png'
+import left_arrow from '../../../public/Images/left_arrow.png'
+
+const HeaderComponent = () => {
+  return (
+    <div className={styles.HeaderComponent_container}>
+      <div className={styles.HeaderComponent_navigation}>
+        <div className={styles.HeaderComponent_top}>
+          <Image src={left_arrow} alt=""></Image>
+          <Image src={menu} alt=""></Image>
+        </div>
+        <div className={styles.HeaderComponent_bottom}>
+          <Image src={line} alt=""></Image>
+        </div>
+      </div>
+
+      <Card />
+    </div>
+  )
+}
+
+export default HeaderComponent
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Catalogue from './components/Catalogue/page'
 import Footer from './components/Footer/page'
+import HeaderComponent from './components/HeaderComponent/page'
 const page = () => {
   return (
    <div className="maincontainer">
+     <HeaderComponent/>
     <Catalogue/>
     <Footer/>
    </div>
