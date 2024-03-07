11b088e78c7c5b01fed21dc6c22a7310f699a720
Aniyeshu-31 <aniyeshu31@gmail.com>
2024-03-08 01:29:30 +0530

    updated!!

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Catalogue from './components/Catalogue/page'
+import Footer from './components/Footer/page'
+import HeaderComponent from './components/HeaderComponent/page'
 
 const page = () => {
-  return (
-    <div>page</div>
+  return(
+       <> 
+       <HeaderComponent/>
+       <Catalogue/>
+       <Footer/>
+       </>
   )
 }
 
